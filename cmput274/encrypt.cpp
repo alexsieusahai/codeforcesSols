520f59bc60c94982757db80f0405ed8156bc6b35
Alexander Sieusahai <alexsieu14@gmail.com>
2017-09-15 19:11:25 -0600

    more done

--- cmput274/encrypt.cpp
+++
@@ -1,6 +0,0 @@
-//bit of code for encryption
-//
-int main()  {
-    encrypt();
-    return 0;
-}
--- projectEuler/problem37.py
+++ projectEuler/problem37.py
@@ -1,38 +1,59 @@
 
-def breakNum(n):
-    n = list(n)
-    numList = []
-    while len(n) >= 1:
-        numList.append(str(n))
+def genDict(upBnd,primeDict,n):
+    # fill the dict with upBnd and n range, assume all prime
+    checkList = []
+    for i in range(upBnd+1,n+1):
+        primeDict[i] = True
+        checkList.append(i)
+    # sieve out using the primes in primeDict
+    for num in primeDict:
+        if num > upBnd:
+            continue
+        if primeDict[num]:
+            # set a to be one below lower bound
+            i = upBnd//num
+            a = i*num
+            while a <= n:
+                if primeDict[a] and a < upBnd:
+                    a += num
+                    continue
+                primeDict[a] = False
+                a += num
+    for num in checkList:
+        if primeDict[num] == False:
+            checkList.remove(num)
+    return [primeDict,n,checkList]
+    
+def getTrunc(n):
+    truncList = []
+    a = n
+    n = list(str(n))
+    a = list(str(a))
+    if len(n) == 1:
+        return [1]
+    while len(n) > 0:
+        truncList.append(int(''.join(n)))
         n.remove(n[0])
-    return numList
+    while len(a) > 0:
+        truncList.append(int(''.join(a)))
+        a.pop()
+    return truncList
+    
 
-def mergeSort(alist):
-    if len(alist) > 1:
-        mid = len(alist)//2
-        left = alist[:mid]
-        right = alist[mid:]
-        mergeSort(left)
-        mergeSort(right)
-        [i,j,k] = [0,0,0]
-        while i < len(left) and j < len(right):
-            if left[i] > right[j]:
-                alist[k] = right[j]
-                j += 1
-            else:
-                alist[k] = left[i]
-                i += 1
-            k += 1
-        while i < len(left):
-            alist[k] = left[i]
-            i += 1
-            k += 1
-        while j < len(right):
-            alist[k] = right[j]
-            j += 1
-            k += 1
 
-# trying mergeSort
-alist =[4,6,1,3]
-mergeSort(alist)
-print(alist)
+truncPrimes = []
+n = 0
+upBnd = 3
+primeDict = {1:False,2:True,3:True,0:False}
+while len(truncPrimes) <= 11:
+    n += 100
+    [primeDict,upBnd,checkList] = genDict(upBnd,primeDict,n)
+    for num in checkList:
+        winner = True
+        for i in getTrunc(num):
+            if primeDict[i] == False:
+                winner = False
+        if winner == True:
+            truncPrimes.append(num)
+            print(truncPrimes)
+print(sum(truncPrimes))
